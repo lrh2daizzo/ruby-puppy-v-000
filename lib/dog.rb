6a4c0d48d0503f63ff8db9ae888a08a7a6bfdbef
Luis Herrera <herrera.lrh@gmail.com>
2019-12-12 02:54:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,5 @@
 
 class Dog
-  @@all = []
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.clear_all
-    all.clear
-  end
-
-  def self.all
-    @@all.each {|dog| puts "#{dog.name}"}
-  end
+  
 
 end
